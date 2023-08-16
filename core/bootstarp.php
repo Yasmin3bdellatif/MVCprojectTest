446eadb09ae4b8dfc0065b4564bdfc047be9d9c0
Yasmeen Abdellatif <112444332+Yasmin3bdellatif@users.noreply.github.com>
2023-08-17 00:07:52 +0200

    update2

--- core/bootstarp.php
+++ core/bootstarp.php
@@ -1,7 +1,14 @@
 <?php
 
+use core\database\Connection;
+
+$app['config']=require "config.php";
+Connection::make($app['config']['database']);
+
 function view($file, $data) {
 
     extract ($data);
     return require "app/views/$file.view.php";
 }
+
+
